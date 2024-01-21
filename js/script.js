5fc9909fc6585d2c85253f710a624a8a19d43271
martinr95 <mrunggaldier95@gmail.com>
2024-01-21 13:38:14 +0100

    add switch for testversion with limited minutes and seconds or real version

--- js/script.js
+++ js/script.js
@@ -14,12 +14,14 @@ let defaultOverlayOpacity = 1;
 //
 //SET LEVEL (get it from localstorage -so like a savefile - or display 0)
 let score = localStorage.getItem("level") || 0;
-
+let targetMinutes = 25;
+let targetSeconds = 60;
 let seconds = 0;
 let minutes = 0;
 let displayLevel = document.getElementById("displayLevel");
 displayLevel.innerHTML = "Level" + " " + score;
 // OTHER LEVEL BASED
+//let testSwitch = document.querySelector(".switch");
 let userTitle = localStorage.getItem("userStoryTitle") || "woodlands";
 console.log(userTitle);
 let userStoryTitle = userTitle;
@@ -55,21 +57,44 @@ let currentAdventureId = userStoryId + score + userBranch + userAdventureNumber;
 // FUNCTIONS
 //
 //
+var switches = document.querySelectorAll('.switch input[type="checkbox"]');
+
+// Add an event listener to each switch
+switches.forEach(function (switchElement) {
+  switchElement.addEventListener("change", function () {
+    // This function is called whenever a switch is flipped
+
+    // The 'this' keyword refers to the checkbox that triggered the event
+    if (this.checked) {
+      console.log(
+        "Test mode is On, target seconds are now 04 and target minutes are 2"
+      );
+      targetSeconds = 4;
+      targetMinutes = 2;
+    } else {
+      console.log("Test mode is OFF. Target seconds: 60, Target minutes 25");
+      targetSeconds = 60;
+      targetMinutes = 25;
+    }
+  });
+});
+
 function stopwatch() {
   seconds++;
   localStorage.setItem("seconds", seconds);
   localStorage.setItem("minutes", minutes);
   seconds = localStorage.getItem("seconds");
   minutes = localStorage.getItem("minutes");
-  if (seconds == 4) {
+  if (seconds == targetSeconds) {
     seconds = 0;
     minutes++;
 
     defaultOverlayOpacity = defaultOverlayOpacity - 0.01;
     storyOverlay.style.opacity = defaultOverlayOpacity;
+    /*
     console.log(defaultOverlayOpacity);
-    console.log(storyOverlay.style.opacity);
-    if (minutes == 2) {
+    console.log(storyOverlay.style.opacity);*/
+    if (minutes == targetMinutes) {
       score++;
       displayLevel.innerHTML = "Level" + " " + score;
       localStorage.setItem("level", score);
@@ -108,7 +133,7 @@ function watchStart() {
 
 function watchStop() {
   clearInterval(timer);
-  console.log(adventuresData.woodlands);
+  //console.log(adventuresData.woodlands);
 }
 
 function watchReset() {
@@ -148,7 +173,7 @@ function setAdventureId() {
 
 function updateAdventure() {
   //Background
-  console.log(adventuresData[userStoryTitle]);
+  // console.log(adventuresData[userStoryTitle]);
   let currentBackground =
     adventuresData[userStoryTitle].adventuresInside[currentAdventureId]
       .adventureimgPortrait;
@@ -180,7 +205,7 @@ pauseBtn.addEventListener("click", function () {
 resetBtn.addEventListener("click", function () {
   watchReset();
   changeScore();
-  console.log(seconds, minutes);
+  // console.log(seconds, minutes);
 });
 exploreBtn.addEventListener("click", function () {
   watchReset();
